8b39806ed454bb38aa2f89df026249406952cafd
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-04-04 18:28:05 -0300

    refactor: separate the server from the routes

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import * as express from "express"
+import { Request, Response } from "express"
+import cors = require("cors")
+import swaggerUi = require("swagger-ui-express")
+import swaggerDocument = require("../swagger.json")
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.use("/v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+server.get("/v1/swagger", (req: Request, res: Response) => {
+  return res.sendFile(process.cwd() + "/swagger.json")
+})
+server.get("/v1/docs", (req: Request, res: Response) => {
+  return res.sendFile(process.cwd() + "/index.html")
+})
+server.get("/v1", (req: Request, res: Response) =>
+  res.status(200).json({ status: "OK" }),
+)
+
+export default server
