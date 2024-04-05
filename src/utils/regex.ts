9d01b806db4668c40bf036b39159c03a34348d8c
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-04-05 18:00:09 -0300

    feat: add regex for field validation

---
+++ src/utils/regex.ts
@@ -0,0 +1,4 @@
+export const fullNameRegex = /^[a-zA-Z]+( [a-zA-Z]+)*$/
+export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+export const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~=-])[A-Za-z\d!@#$%^&*()_+{}[\]:;<>,.?~=-]{8,}$/
