d29ca1c5578caf77fd6bf387000bb1af47b5b686
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-04-05 18:03:01 -0300

    feat: add model for user creation and search

---
+++ src/models/user.model.ts
@@ -0,0 +1,41 @@
+import { model, Schema } from "mongoose"
+import bcrypt = require("bcrypt")
+import { UserDocument } from "../types/types"
+
+class UserModel {
+  private userModel = model<UserDocument>(
+    "User",
+    new Schema<UserDocument>(
+      {
+        fullName: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        isAdmin: {
+          type: Boolean,
+          default: false,
+        },
+      },
+      { timestamps: true },
+    ),
+  )
+
+  public async createUser(
+    fullName: string,
+    email: string,
+    password: string,
+  ): Promise<UserDocument> {
+    const hashedPassword = bcrypt.hashSync(password, 10)
+    const newUser = new this.userModel({
+      fullName,
+      email,
+      password: hashedPassword,
+    })
+    return await newUser.save()
+  }
+
+  public async findUserByEmail(email: string): Promise<UserDocument | null> {
+    return await this.userModel.findOne({ email })
+  }
+}
+
+export const userModel = new UserModel()
